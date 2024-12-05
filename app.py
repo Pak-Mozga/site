9a6312efbc6d2348fcf4d32ed2f900258c8d4929
packard2x2 <126377090+packard2x2@users.noreply.github.com>
2024-12-05 17:24:26 +0300

    add html boot

--- app.py
+++ app.py
@@ -2,14 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template("index.html")
+@app.route('/')
+def index() -> str:
+    return render_template('starter-page.html')
 
-
-@app.route("/about")
-def get_page_about():
-    return render_template("about.html")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
